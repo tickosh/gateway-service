3a9904fae8ba3b99d7cab02b35041aa091a44fdc
Duman <201605@astanait.edu.kz>
2025-03-17 20:21:34 +0500

    init

---
+++ src/main/java/kz/tickosh/gateway/GatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package kz.tickosh.gateway;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(GatewayServiceApplication.class)
+				.web(WebApplicationType.REACTIVE)
+				.run(args);	}
+
+}
